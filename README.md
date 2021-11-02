25d4b3da4a609aa047df9a64e92efe20fdb720c8
Kate Hamilton <kate.hamilton828@gmail.com>
2021-11-02 22:31:40 +0300

    final changes

--- README.md
+++ README.md
@@ -28,7 +28,7 @@ Developed by: Kate Hamilton
 
 
 
-
+#Requirements 
 GIVEN a weather dashboard with form inputs
 WHEN I search for a city
 THEN I am presented with current and future conditions for that city and that city is added to the search history
--- assets/js/script.js
+++ assets/js/script.js
@@ -11,9 +11,8 @@ citySearchEL.addEventListener("submit", function (event) {
   console.log(userInputEL.value);
   getCityWeather(userInputEL.value);
   save(userInputEL.value);
-  userInputEL.value = ""
-  // resetDisplay()
-  // displaySearchHistory()
+  userInputEL.value = "";
+
 });
 
 
@@ -34,7 +33,6 @@ var getCityWeather = function (city) {
       response.json().then(function (data) {
         console.log("display Weather", data);
         getForcast(data.coord.lat, data.coord.lon);
-
       });
     }
   });
@@ -43,58 +41,46 @@ var getCityWeather = function (city) {
 var getForcast = function (lat, lon) {
   var apiKey = "147201a46f94fdc2b6ce9d902d685f99";
   var apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`;
-
+ 
   fetch(apiURL).then(function (response) {
     if (response.ok) {
       response.json().then(function (data) {
         console.log("current data", data)
+      
         var imgContainer = document.getElementById("img");
-        console.log(imgContainer);
         var newImg = document.createElement("img");
-        console.log(newImg);
+        imgContainer.innerHTML = "";
         newImg.setAttribute("src", `http://openweathermap.org/img/wn/${data.current.weather[0].icon}@4x.png`)
         imgContainer.appendChild(newImg);
         document.getElementById("temperature").innerHTML = Math.round((data.current.temp) - 273.15) * 1.8 + 32 + "°F";
         document.getElementById("UV-index").innerHTML = "UV index: " + data.current.uvi;
-        
-       if (data.current.uvi <= 2) {
-        currentUVEL.setAttribute("class", "favorable");
+        if (data.current.uvi <= 2) {
+          currentUVEL.setAttribute("class", "favorable");
         }
         else if (data.current.uvi > 2 && data.current.temp <= 5) {
           currentUVEL.setAttribute("class", "moderate");
         }
-        else  {
+        else {
           currentUVEL.setAttribute("class", "severe");
-        }
-    
-
+        };
         document.getElementById("wind-span").innerHTML = "Wind: " + data.current.wind_speed + "MPH";
         document.getElementById("humidity-span").innerHTML = "Humidity: " + data.current.humidity + "%";
         makeForecast(data);
-        
 
       });
     };
   });
 };
 
-
-
-
-
-
-
 function makeForecast(data) {
-
+ 
   console.log(data);
   let anchorEl = document.getElementById("weather")
+  anchorEl.innerHTML = "";
   for (let i = 0; i < 5; i++) {
     let myDate = new Date(data.daily[i].dt * 1000).toLocaleDateString("en-us");
-
-    anchorEl.innerHTML += `  
-  <div class="daily-block"
-        id=${i + 1}">
-        <p class="date">${myDate} </p>
+  
+    anchorEl.innerHTML += `<div class="daily-block" id=${i + 1}"><p class="date">${myDate} </p>
         <img src="http://openweathermap.org/img/wn/${data.current.weather[0].icon}@4x.png"> </img>
     <p class="temp"> ${Math.round((data.daily[i].temp.day) - 273.15) * 1.8 + 32}°F</p>
     <p class="wind">Wind:${data.daily[i].wind_speed} MPH </p>
@@ -104,8 +90,6 @@ function makeForecast(data) {
   };
 };
 
-
-
 function save(cityname) {
   var previousSearches = JSON.parse(localStorage.getItem('recentCities')) || [];
   previousSearches.push(cityname);
@@ -113,9 +97,7 @@ function save(cityname) {
   renderRecentCities()
   cityList()
 }
-
 // create a for loop that loops over the array. 
-
 
 function renderRecentCities() {
   var recentCities = JSON.parse(localStorage.getItem('recentCities')) || [];;
@@ -126,10 +108,9 @@ function renderRecentCities() {
 }
 
 function cityList() {
+
   var recentCities = JSON.parse(localStorage.getItem('recentCities'));
-
   for (var i = 0; i < recentCities.length; i++) {
-
     var newList = document.createElement("button");
     newList.classList.add("historyButton");
 
@@ -141,16 +122,6 @@ function cityList() {
       getCityWeather(event.target.innerHTML)
       userInputEL.value = ""
     });
-
-
-  }
-}
-
+  };
+};
 cityList()
-
-
-// function resetDisplay() {
-//   imgContainer.appendChild(newImg)= ""
-//   anchorEl = ""
-//   cityContainerEl = ""
-// }
